86500ca49dde20255d057be9f7785e4ec3f140f1
Shengyu Zhang <lastavengers@outlook.com>
2016-12-14 01:04:37 +0800

    parse http request line

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,87 @@ use std::net::{TcpStream, TcpListener};
 static WELCOME: &'static str = "HTTP/1.0 200 OK\r\n\
      Content-type: text/html; charset=utf-8\r\n\
      \r\n\
-     <html>\
-        <head>\
+     <html>
+        <head>
             <title>君の名は!</title>
-        </head>\
+        </head>
         <body>
             <center>
-                <h1>Welcome!</h1>\
-                <p>君の名は!</p>\
+                <h1>Welcome!</h1>
+                <p>君の名は!</p>
             </center>
         </body>
      </html>
      \0";
 
-fn handle_client(mut stream: TcpStream) {
+struct HttpRequest {
+    /* request line */
+    method: String,
+    rawuri: String,
+    version: String,
+    // args
+
+    /* request header */
+    // header: HttpHeader,
+    content_type: String,
+    content_length: u32,
+
+    /* request body */
+    body: String,
+}
+
+fn accept_request(mut stream: TcpStream) {
     let mut buf = [0; 512];
+    // let mut req: HttpRequest = HttpRequest{ .. };
+
     stream.read(&mut buf).unwrap();
 
     let s = match str::from_utf8(&buf) {
         Ok(v) => v,
         Err(e) => panic!("invaild utf-8 sequcence {}", e),
     };
-    println!("recv: {}", s);
+
+    let mut line = s.split("\r\n");
+
+    /* read request line */
+    let req_line = match line.next() {
+        Some(v) => v,
+        None => panic!("no request line found"),
+    };
+    println!("request line: {}", req_line);
+
+    let mut req_line =  req_line.split_whitespace();
+
+    let method = match req_line.next() {
+        Some(v) => {
+            if v != "POST" && v != "GET" {
+                panic!("unsupported method");
+            } else {
+                v
+            }
+        },
+        None => panic!("no method found in request line"),
+    };
+    println!("method: {}", method);
+
+    let rawuri = match req_line.next() {
+        Some(v) => v,
+        None => panic!("no uri found in request line"),
+    };
+    println!("rawuri: {}", rawuri);
+
+    let version = match req_line.next() {
+        Some(v) => v,
+        None => panic!("no version found in request line"),
+    };
+    println!("version: {}", version);
+
+    /* read request header */
+    let req_header = match line.next() {
+        Some(v) => v,
+        None => panic!("no request header found"),
+    };
+    println!("request header: {}", req_header);
 
     stream.write(WELCOME.as_bytes()).unwrap();
 }
@@ -43,7 +102,7 @@ fn main() {
     for stream in listenser.incoming() {
         match stream {
             Ok(stream) => {
-                handle_client(stream);
+                accept_request(stream);
             }
             Err(e) => {
                 panic!(e);
